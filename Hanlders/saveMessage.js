330dbb04cbf3fb78785ee842d1ffc1175cc87988
Debarun <paldebarun27@gmail.com>
2024-01-17 01:49:40 +0530

    the handler to send mail is created

--- Hanlders/saveMessage.js
+++ Hanlders/saveMessage.js
@@ -30,17 +30,17 @@ exports.saveMessage = async (req, res) => {
         let info = await transporter.sendMail({
             from: process.env.MAIL_USER,
             to: email,
-            subject: "job applied",
+            subject: "Thank You for Your Inquiry – Dev Bhumi Ratan Herbal",
          html:`
          <div style="background-color: #ffffff; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 10px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
          <h2 style="color: #3d6c40;">AyurWell - Inquiry Response</h2>
          <p>Dear ${firstName} ${lastName},</p>
-         <p>Thank you for reaching out to AyurWell with your inquiry. We appreciate your interest in our Ayurvedic solutions for well-being.</p>
+         <p>Thank you for reaching out to Dev Bhumi Ratan Herbal with your inquiry. We appreciate your interest in our Ayurvedic solutions for well-being.</p>
          <p>We have received your message and want to assure you that our team is diligently reviewing your inquiry. If your needs align with our offerings, a member of our AyurWell team will be in touch with you soon to provide further information and assistance.</p>
          <p>Should you have any immediate questions or require additional details, please don't hesitate to contact us.</p>
-         <p>Thank you once again for considering AyurWell as your Ayurvedic partner on your journey to wellness.</p>
+         <p>Thank you once again for considering Dev Bhumi Ratan Herbal as your Ayurvedic partner on your journey to wellness.</p>
          <p>Best regards,</p>
-         <p style="font-weight: bold;">AyurWell Team,<br>AyurWell Enterprises</p>
+         <p style="font-weight: bold;">AyurWell Team,<br>Dev Bhumi Ratan Herbal</p>
      </div>
      
             `,
